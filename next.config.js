2c43051c2e4aaa1c48af21c5045e01cc37a66c81
f9260 <wtfthisvideo@gmail.com>
2024-06-13 17:45:41 +0300

    olacak

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+/** @type {import('next').NextConfig} */
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra({
-  basePath: '/tr/rehber',
-}
-)
+
+const nextConfig = {
+
+  images: {
+    unoptimized: true,
+  },
+
+};
+
+module.exports = {
+  ...withNextra(),
+  ...nextConfig,
+};
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,23 +1,51 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+import rehber from 'images/rehber.png'
+
 
 const config: DocsThemeConfig = {
-  logo: <span>VE3.Plus</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <Image src={rehber} alt=""/>,
+
+  docsRepositoryBase: 'https://github.com/shuding/-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <span>
+         {new Date().getFullYear()} ©{' '}
+        <a href="https://ve3.plus/tr" target="_blank">
+          VE3.Plus
+        </a>
+        
+      </span>
+    )
+
   },
     toc: {
     title: "İçindekiler",
     // Diğer TOC bileşeni özellikleri...
   },
   
+  editLink: {
+    component: null,
+    // Diğer TOC bileşeni özellikleri...
+  },
+  feedback: {
+    content: "Bir sorun mu var? Bizimle paylaş",
+    useLink: () =>  "https://forum.ve3.plus/t/q-a"
+  // Diğer TOC bileşeni özellikleri...
+  },
+
+  search : {
+    placeholder:"Rehber'de Ara"
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – VE3.Plus | Rehber'
+    }
+  },
+  gitTimestamp: null,
+
+
   primaryHue: 170
 }
 
